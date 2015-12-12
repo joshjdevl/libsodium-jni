2009a43ed775a596db927eea699980e46c61ceda
josh <joshjdevl@gmail.com>
2015-12-12 14:18:30 -0500

    fix path

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NDK_ROOT=/android-ndk-r10e-linux-x86_64
+export NDK_ROOT=/installs/android-ndk-r10e-linux-x86_64
 export PATH=/installs/android-toolchain/bin:${NDK_ROOT}:$PATH
 export PATH=${NDK_ROOT}:$PATH
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
