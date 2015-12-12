0791a03be4c7355ca296f494592325fa629152f0
josh <joshjdevl@gmail.com>
2015-12-12 13:54:14 -0500

    fix typos

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-export NDK_ROOT /installs/android-ndk-r9d
-export PATH /installs/android-toolchain/bin:${NDK_ROOT}:$PATH
-export PATH ${NDK_ROOT}:$PATH
-export JAVA_HOME /usr/lib/jvm/java-7-oracle
-export ANDROID_NDK_HOME ${NDK_ROOT}
+export NDK_ROOT=/android-ndk-r10e-linux-x86_64
+export PATH=/installs/android-toolchain/bin:${NDK_ROOT}:$PATH
+export PATH=${NDK_ROOT}:$PATH
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export ANDROID_NDK_HOME=${NDK_ROOT}
