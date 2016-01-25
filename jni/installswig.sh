2db402e5828657773f57c8c1b6ed235b9dde7984
josh <joshjdevl@gmail.com>
2016-01-24 19:32:33 -0500

    build

--- jni/installswig.sh
+++ jni/installswig.sh
@@ -5,8 +5,9 @@ set -e
 if uname -a | grep -q -i darwin; then
   brew install swig
 else
+    rm -rf swig*
     VERSION=3.0.8
-    wget http://prdownloads.sourceforge.net/swig/swig-${VERSIN}.tar.gz
+    wget http://prdownloads.sourceforge.net/swig/swig-${VERSION}.tar.gz
     tar -xvf swig-${VERSION}.tar.gz
     cd swig-${VERSION}
     ./configure
