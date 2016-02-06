c3cf160daf309d7a96601b492ff50429a95640d7
joshjdevl <joshjdevl@gmail.com>
2016-02-06 15:48:27 -0800

    cleanup

--- jni/compile.sh
+++ jni/compile.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 if [ -z "$JAVA_HOME" ]; then
@@ -14,7 +16,6 @@ rm -f *.java
 rm -f *.c
 rm -f *.so
 
-#swig -java sodium.i
 swig -java -package org.libsodium.jni -outdir ../src/main/java/org/libsodium/jni sodium.i
 
 
@@ -33,17 +34,6 @@ echo $destlib/$jnilib
 
 sudo cp /usr/local/lib/libsodium.* /usr/lib
 
-#In order to compile for arm/armv7/x86/mips you should build your own standalone android-toolchain as in libsodium:android-build.sh
-#https://github.com/jedisct1/libsodium/blob/master/dist-build/android-build.sh
-#And then use gcc binary from there. 
-#Example(arm):
-#/installs/libsodium/android-toolchain-arm/arm-linux-androideabi/bin/gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I/installs/libsodium/libsodium-android-arm/include sodium_wrap.c -shared -fPIC -L/installs/libsodium/libsodium-android-arm/lib -lsodium -o $jnilib
-#Example(arm7):
-#/installs/libsodium/android-toolchain-armv7/arm-linux-androideabi/bin/gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I/installs/libsodium/libsodium-android-armv7/include sodium_wrap.c -shared -fPIC -L/installs/libsodium/libsodium-android-armv7/lib -lsodium -o $jnilib
-#Example(mips):
-#/installs/libsodium/android-toolchain-mips/mipsel-linux-android/bin/gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I/installs/libsodium/libsodium-android-mips/include sodium_wrap.c -shared -fPIC -L/installs/libsodium/libsodium-android-mips/lib -lsodium -o $jnilib
-#Example(x86):
-#/installs/libsodium/android-toolchain-x86/i686-linux-android/bin/gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I/installs/libsodium/libsodium-android-x86/include sodium_wrap.c -shared -fPIC -L/installs/libsodium/libsodium-android-x86/lib -lsodium -o $jnilib
 gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux sodium_wrap.c -shared -fPIC -L/usr/lib -lsodium -o $jnilib
 sudo rm -f $destlib/$jnilib  
 sudo cp $jnilib $destlib
