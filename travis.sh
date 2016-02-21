ce19e09f65274f42396f02349b33718c276673f7
joshjdevl <joshjdevl@gmail.com>
2016-02-20 20:04:49 -0800

    build linux

--- travis.sh
+++ travis.sh
@@ -3,6 +3,5 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     ./build-mac.sh > /dev/null 2>&1
 else
-    ./dependencies-linux.sh > /dev/null 2>&1
-    ./build.sh > /dev/null 2>&1
+    ./build-linux.sh > /dev/null 2>&1
 fi
