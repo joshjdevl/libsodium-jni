75a4deadb5790198e0da662d51b77eb1b006cf14
joshjdevl <joshjdevl@gmail.com>
2016-02-20 18:53:18 -0800

    setenv

--- build-libsodiumjni.sh
+++ build-libsodiumjni.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -ev
 
+if uname -a | grep -q -i darwin; then
+    echo "do nothing"
+else
+    . ./setenv.sh
+fi
+
 ndk-build
 rm -rf src/main/jniLibs/
 cp -R libs src/main/jniLibs
--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export NDK_ROOT=/installs/android-ndk-r10e
-export PATH=/installs/android-toolchain/bin:${NDK_ROOT}:$PATH
-export PATH=${NDK_ROOT}:$PATH
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export ANDROID_NDK_HOME=${NDK_ROOT}
+if uname -a | grep -q -i darwin; then
+    echo "do nothing"
+else
+    export NDK_ROOT=/installs/android-ndk-r10e
+    export PATH=/installs/android-toolchain/bin:${NDK_ROOT}:$PATH
+    export PATH=${NDK_ROOT}:$PATH
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+    export ANDROID_NDK_HOME=${NDK_ROOT}
+fi
+
