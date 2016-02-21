d1793ab7048181ae49525f15ed0e96ca17579ac7
joshjdevl <joshjdevl@gmail.com>
2016-02-20 21:30:29 -0800

    more android builds

--- build.sh
+++ build.sh
@@ -19,8 +19,11 @@ sudo make install
 
 ./dist-build/android-arm.sh
 ./dist-build/android-armv7-a.sh
+./dist-build/android-armv8-a.sh
 ./dist-build/android-mips32.sh
+./dist-build/android-mips64.sh
 ./dist-build/android-x86.sh
+./dist-build/android-x86-64.sh
 
 popd
 
