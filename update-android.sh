0074a56740a96e1dc1c77f64da4eb7ce3ab9581b
joshjdevl <joshjdevl@gmail.com>
2016-09-02 16:47:27 -0500

    android build tools and sdk version

--- update-android.sh
+++ update-android.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ev
 
 echo y | android update sdk --all --filter build-tools-23.0.2 --no-ui
+echo y | android update sdk --all --filter build-tools-23.0.3 --no-ui
 echo y | android update sdk --all --filter android-16 --no-ui
+echo y | android update sdk --all --filter android-21 --no-ui
 echo y | android update sdk --all --filter android-23 --no-ui
+echo y | android update sdk --all --filter android-24 --no-ui
