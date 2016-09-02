036f7fda61fe9ad5b9d8e516e3471ad09fbb8551
joshjdevl <joshjdevl@gmail.com>
2016-09-02 06:30:59 -0500

    disable dev null mac build

--- travis.sh
+++ travis.sh
@@ -2,9 +2,9 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     #./build-mac.sh > /dev/null
-    ./dependencies-mac.sh > /dev/null
-    ./build.sh > /dev/null
-    ./build-kaliumjni.sh > /dev/null
+    ./dependencies-mac.sh
+    ./build.sh
+    ./build-kaliumjni.sh
 else
     ./build-linux.sh
 fi
