80c55ce1a3e79a3ae51de43ff60b4825fc61dc8b
joshjdevl <joshjdevl@gmail.com>
2016-09-02 04:26:18 -0500

    brew install and brew upgrade

--- dependencies-mac.sh
+++ dependencies-mac.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -ev
 
-brew install libtool autoconf automake swig android-sdk android-ndk gradle libsodium maven
+#brew upgrade libtool autoconf automake swig android-sdk android-ndk gradle libsodium maven
+#https://stackoverflow.com/questions/20802320/detect-if-homebrew-package-is-installed/20802381#20802381
+for pkg in libtool autoconf automake swig android-sdk android-ndk gradle libsodium maven; do
+    if brew list -1 | grep -q "^${pkg}\$"; then
+        echo "Package '$pkg' is installed"            
+        #brew upgrade $pkg
+    else
+        echo "Package '$pkg' is not installed"            
+        brew install $pkg
+    fi
+done
+
+brew upgrade libtool autoconf automake swig android-sdk android-ndk gradle libsodium maven
 
 ./update-android.sh
