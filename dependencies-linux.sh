bf6676896d69d86df2f182b50b88061cb9fe72a5
joshjdevl <joshjdevl@gmail.com>
2016-09-02 05:42:28 -0500

    qq before command

--- dependencies-linux.sh
+++ dependencies-linux.sh
@@ -6,7 +6,7 @@ set -ev
 
 sudo apt-get -qq update && sudo apt-get -y -qq install python-software-properties software-properties-common
 sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-sudo apt-get update -qq
+sudo apt-get -qq update
 
 sudo add-apt-repository ppa:saiarcot895/myppa
 sudo apt-get update
@@ -14,13 +14,13 @@ sudo apt-get -y -qq install apt-fast
 
 sudo apt-fast update
 
-sudo apt-fast install -y -qq wget git autoconf autoconf automake build-essential autogen libtool gettext-base gettext vim bzip2 libpcre3-dev libpcre++-dev pkg-config
+sudo apt-fast -y -qq install wget git autoconf autoconf automake build-essential autogen libtool gettext-base gettext vim bzip2 libpcre3-dev libpcre++-dev pkg-config
 #gradle building issues
-sudo apt-fast install -y -qq gcc-multilib lib32z1
+sudo apt-fast -y -qq install gcc-multilib lib32z1
 sudo add-apt-repository ppa:webupd8team/java -y 
-sudo apt-fast update -qq
+sudo apt-fast -qq update 
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-fast install -y -qq oracle-java8-installer maven
+sudo apt-fast -y -qq install oracle-java8-installer maven
 
 mkdir -p ./installs
 pushd ./installs
