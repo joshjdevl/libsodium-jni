c2334d7cd98e55c1a3671a7310b9dcfd055b7057
joshjdevl <joshjdevl@gmail.com>
2016-12-21 05:30:02 +0000

    blake2b hash tests

--- src/test/java/org/libsodium/jni/crypto/HashTest.java
+++ src/test/java/org/libsodium/jni/crypto/HashTest.java
@@ -29,6 +29,25 @@ import static org.junit.Assert.fail;
 
 public class HashTest {
 
+    /**
+     * Blake2 test vectors
+     */
+
+    public static final String Blake2_Message="Hello world";
+    public static final String Blake2_HEXDIGEST="6ff843ba685842aa82031d3f53c48b66326df7639a63d128974c5c14f31a0f33343a8c65551134ed1ae0f2b0dd2bb495dc81039e3eeb0aa1bb0388bbeac29183";
+
+    public static final String Blake2_DIGEST = "01718cec35cd3d796dd00020e0bfecb473ad23457d063b75eff29c0ffa2e58a9";
+    public static final String Blake2_DIGEST_EMPTY_STRING = "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce";
+    public static final String Blake2_DIGEST_NULL = "2fa3f686df876995167e7c2e5d74c4c7b6e48f8068fe0e44208344d480f7904c36963e44115fe3eb2a3ac8694c28bcb4f5a0f3276f2e79487d8219057a506e4b";    
+
+
+    public static final String Blake2_KEY = "This is a super secret key. Ssshh!";
+    public static final String Blake2_SALT = "0123456789abcdef";
+    public static final String Blake2_PERSONAL = "fedcba9876543210";
+    public static final String Blake2_DIGEST_WITH_SALT_PERSONAL = "108e81d0c7b0487de45c54554ea35b427f886b098d792497c6a803bbac7a5f7c";
+
+    public static final String HASH_ERR = "Hash is invalid";
+
     private final Hash hash = new Hash();
 
     @Test
@@ -99,39 +118,39 @@ public class HashTest {
         }
     }
 
-    /*
     @Test
     public void testBlake2() throws Exception {
-        final byte[] rawMessage = Blake2_MESSAGE.getBytes();
+        final byte[] rawMessage = Blake2_Message.getBytes();
         String result = HEX.encode(hash.blake2(rawMessage));
-        assertTrue("Hash is invalid", Arrays.equals(Blake2_DIGEST.getBytes(), result.getBytes()));
+	//System.out.println("testBlake2="+result);
+	assertEquals(HASH_ERR,Blake2_HEXDIGEST,result);
     }
 
     @Test
     public void testBlake2EmptyString() throws Exception {
         byte[] result = hash.blake2("".getBytes());
-        assertEquals("Hash is invalid", Blake2_DIGEST_EMPTY_STRING, HEX.encode(result));
+	//System.out.println("testBlake2EmptyString="+HEX.encode(result));
+	assertEquals(HASH_ERR, Blake2_DIGEST_EMPTY_STRING, HEX.encode(result));
     }
 
     @Test
     public void testBlake2HexString() throws Exception {
-        String result = hash.blake2(Blake2_MESSAGE, HEX);
-        assertEquals("Hash is invalid", Blake2_DIGEST, result);
+        String result = hash.blake2(Blake2_Message, HEX);
+	//System.out.println("testBlake2HexString="+result);
+	assertEquals(HASH_ERR,Blake2_HEXDIGEST,result);
     }
 
     @Test
     public void testBlake2EmptyHexString() throws Exception {
         String result = hash.blake2("", HEX);
-        assertEquals("Hash is invalid", Blake2_DIGEST_EMPTY_STRING, result);
+	//System.out.println("testBlake2EmptyHexString="+result);
+        assertEquals(HASH_ERR, Blake2_DIGEST_EMPTY_STRING, result);
     }
 
     @Test
     public void testBlake2NullByte() {
-        try {
-            hash.blake2("\0".getBytes());
-        } catch (Exception e) {
-            fail("Should not raise any exception on null byte");
-        }
+	byte[] result=hash.blake2("\0".getBytes());
+	//System.out.println("testBlake2NullByte="+HEX.encode(result));
+	assertEquals(HASH_ERR, Blake2_DIGEST_NULL, HEX.encode(result));
     }
-    */
 }
