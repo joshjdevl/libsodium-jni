7ffd4a63f2f55c60d4e1a53e53791aaef5ce88c5
joshjdevl <joshjdevl@gmail.com>
2017-02-06 14:54:41 -0500

    fPIC

--- jni/Android.mk
+++ jni/Android.mk
@@ -41,6 +41,7 @@ endif
 include $(CLEAR_VARS)
 LOCAL_MODULE := sodium
 LOCAL_SRC_FILES := ../libsodium/libsodium-android-$(MY_ARCH_FOLDER)/lib/libsodium.a #/installs/libsodium/libsodium-android-(x86|arm|mips)/lib/libsodium.a
+LOCAL_LDFLAGS  += -fPIC
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -48,7 +49,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := sodiumjni
 LOCAL_SRC_FILES :=  \
 sodium_wrap.c
-
+LOCAL_LDFLAGS  += -fPIC
 LOCAL_CFLAGS   += -Wall -g -pedantic -std=c99
 
 LOCAL_C_INCLUDES += ../libsodium/libsodium-android-$(MY_ARCH_FOLDER)/include ../libsodium/libsodium-android-$(MY_ARCH_FOLDER)/include/sodium /usr/local/include
