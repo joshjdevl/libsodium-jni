00b653a728fdb4c0e6e825437e99d50a2cd0c640
joshjdevl <joshjdevl@gmail.com>
2017-02-06 13:16:08 -0500

    keep logs

--- build.sh
+++ build.sh
@@ -22,19 +22,19 @@ make --quiet && make --quiet check
 sudo make --quiet install
 
 date
-./dist-build/android-arm.sh > /dev/null
+./dist-build/android-arm.sh 
 date
-./dist-build/android-armv7-a.sh > /dev/null
+./dist-build/android-armv7-a.sh 
 date
-./dist-build/android-armv8-a.sh > /dev/null 
+./dist-build/android-armv8-a.sh
 date
-./dist-build/android-mips32.sh > /dev/null
+./dist-build/android-mips32.sh
 date
-./dist-build/android-mips64.sh > /dev/null
+./dist-build/android-mips64.sh
 date
-./dist-build/android-x86.sh > /dev/null
+./dist-build/android-x86.sh 
 date
-./dist-build/android-x86_64.sh > /dev/null
+./dist-build/android-x86_64.sh
 date
 #folder is being named westmere for the x86-64 architecture
 mv libsodium-android-westmere libsodium-android-x86-64
