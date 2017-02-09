e52a912349f10afb963eda20c44117a23efbb780
Felix von Ferey <felix+libsodium-jni@noreply.ferey.de>
2017-02-09 11:48:51 +0100

    Also install "extra-android-m2repository".

--- update-android.sh
+++ update-android.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ev
 
+echo y | android update sdk --all --filter extra-android-m2repository --no-ui
 echo y | android update sdk --all --filter build-tools-23.0.2 --no-ui
 echo y | android update sdk --all --filter build-tools-23.0.3 --no-ui
 echo y | android update sdk --all --filter build-tools-25.0.2 --no-ui
