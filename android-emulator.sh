c01fee152670dd5970530cda810c0662e761c885
Felix von Ferey <felix+libsodium-jni@noreply.ferey.de>
2017-02-24 21:28:49 +0100

    Work around "adb shell" hang.

--- android-emulator.sh
+++ android-emulator.sh
@@ -8,5 +8,12 @@ android list targets
 echo no | android create avd --force -n test -t android-10 --abi armeabi-v7a
 emulator -avd test -no-window -memory 512 -wipe-data 2>&1 | tee emulator.log &
 adb logcat 2>&1 | tee logcat.log &
-bash -x "$(which android-wait-for-emulator)"
+
+# Workaround from https://code.google.com/p/android/issues/detail?id=10255#c31 to prevent the hanging of "adb shell"
+while true; do 
+#	echo "Pinging ADB server."
+	adb -e shell echo ping || true
+	sleep 10
+done &
+
 android-wait-for-emulator
