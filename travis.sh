910c20bfb3ef8cc11b9aa49b4a5469bce6621198
joshjdevl <joshjdevl@gmail.com>
2017-02-26 11:13:12 -0800

    enable osx

--- travis.sh
+++ travis.sh
@@ -3,9 +3,11 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     #./build-mac.sh > /dev/null
     ./dependencies-mac.sh
-    ./build.sh
-    ./build-kaliumjni.sh
+    #./build.sh
+    #./build-kaliumjni.sh
 else
-    true # Building for Linux is now fully implemented by "gradle build". 
+    #true # Building for Linux is now fully implemented by "gradle build". 
+    gradle build --full-stacktrace
+    gradle connectedCheck --full-stacktrace
 fi
 
