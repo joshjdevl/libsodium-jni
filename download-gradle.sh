f647d5254698093fd8e3711cdfbf68d4360f1410
joshjdevl <joshjdevl@gmail.com>
2017-03-23 00:52:38 -0700

    license

--- download-gradle.sh
+++ download-gradle.sh
@@ -9,3 +9,6 @@ test -e "gradle-${GRADLE_VERSION}" || unzip -qq "gradle-${GRADLE_VERSION}"-bin.z
 wget --quiet https://dl.google.com/android/repository/tools_${ANDROID_SDK_VERSION}-linux.zip
 test -e "tools" || unzip -qq tools_${ANDROID_SDK_VERSION}-linux.zip # Do not overwrite an installed Android SDK, because overwriting it may corrupt it.
 popd
+
+mkdir -p ${ANDROID_HOME}/licenses
+echo 8933bad161af4178b1185d1a37fbf41ea5269c55 > ${ANDROID_HOME}/licenses/android-sdk-license
