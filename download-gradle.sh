b565d990d68c67f5631c35fb8dd50e00fd76b006
joshjdevl <joshjdevl@gmail.com>
2017-03-22 22:22:01 -0700

    android tools new commands

--- download-gradle.sh
+++ download-gradle.sh
@@ -7,5 +7,6 @@ wget --quiet https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-
 test -e "gradle-${GRADLE_VERSION}" || unzip -qq "gradle-${GRADLE_VERSION}"-bin.zip
 
 wget --quiet http://dl.google.com/android/android-sdk_${ANDROID_SDK_VERSION}-linux.tgz
-test -e "android-sdk-linux" || tar -xf android-sdk_${ANDROID_SDK_VERSION}-linux.tgz # Do not overwrite an installed Android SDK, because overwriting it may corrupt it.
+wget --quiet https://dl.google.com/android/repository/tools_${ANDROID_SDK_VERSION}-linux.zip
+test -e "tools" || tar -xf tools_${ANDROID_SDK_VERSION}-linux.zip # Do not overwrite an installed Android SDK, because overwriting it may corrupt it.
 popd
