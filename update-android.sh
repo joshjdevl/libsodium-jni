bca9731be1d926620d250a7cbb94022255d08de3
joshjdevl <joshjdevl@gmail.com>
2017-03-23 00:52:25 -0700

    tools

--- update-android.sh
+++ update-android.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -ev
 
+echo y | sdkmanager "platform-tools"
+echo y | sdkmanager "tools"
+
+
 echo y | sdkmanager "extras;android;m2repository"
+echo y | sdkmanager "extras;google;m2repository"
+
 echo y | sdkmanager "build-tools;23.0.2"
 echo y | sdkmanager "build-tools;23.0.3" 
 echo y | sdkmanager "build-tools;25.0.2" 
+
 echo y | sdkmanager "platforms;android-16" 
 echo y | sdkmanager "platforms;android-21" 
 echo y | sdkmanager "platforms;android-23" 
 echo y | sdkmanager "platforms;android-24" 
 echo y | sdkmanager "platforms;android-25" 
-echo y | sdkmanager "platform-tools" 
-echo y | sdkmanager "tools" 
+
 
