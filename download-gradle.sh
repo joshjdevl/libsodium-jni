d92be542eaf047fc7f64048543ec851d1ae3d954
joshjdevl <joshjdevl@gmail.com>
2017-03-23 01:24:22 -0700

    licenses

--- download-gradle.sh
+++ download-gradle.sh
@@ -12,3 +12,6 @@ popd
 
 mkdir -p ${ANDROID_HOME}/licenses
 echo 8933bad161af4178b1185d1a37fbf41ea5269c55 > ${ANDROID_HOME}/licenses/android-sdk-license
+echo 601085b94cd77f0b54ff86406957099ebe79c4d6 > ${ANDROID_HOME}/licenses/android-googletv-license
+echo 33b6a2b64607f11b759f320ef9dff4ae5c47d97a > ${ANDROID_HOME}/licenses/google-gdk-license
+echo e9acab5b5fbb560a72cfaecce8946896ff6aab9d > ${ANDROID_HOME}/licenses/mips-android-sysimage-license
