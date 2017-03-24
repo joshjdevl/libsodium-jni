512a0caa8ed093f8fbaa146588fc0c945ba6a70c
joshjdevl <joshjdevl@gmail.com>
2017-03-23 19:01:25 -0700

    use build.sh instead of gradle build for now

--- travis.sh
+++ travis.sh
@@ -4,7 +4,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     true
 else
     #true # Building for Linux is now fully implemented by "gradle build". 
-    gradle build --full-stacktrace
+    #gradle build --full-stacktrace
     #gradle connectedCheck --full-stacktrace
+    ./build.sh
 fi
 
