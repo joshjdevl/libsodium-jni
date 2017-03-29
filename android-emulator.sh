9a7a9b3d371d12585d50d15cf8ca29837f76116f
joshjdevl <joshjdevl@gmail.com>
2017-03-29 10:35:18 -0700

    which sdkmanager

--- android-emulator.sh
+++ android-emulator.sh
@@ -7,6 +7,7 @@ ANDROID_ABI="${ANDROID_ABI:-armeabi-v7a}"
 
 cat "$(which android-wait-for-emulator)"
 which android
+which sdkmanager
 while true; do echo y; sleep 3; done | sdkmanager "system-images;${ANDROID_API};default;${ANDROID_ABI}" "platforms;${ANDROID_API}"
 android list targets
 echo no | android create avd --force -n test -k "system-images;${ANDROID_API};default;${ANDROID_ABI}" --abi "${ANDROID_ABI}"
