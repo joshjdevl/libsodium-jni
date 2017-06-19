88f015471dc28d9a08f60f6e6f4dac33be68ac60
Saul Shanabrook <s.shanabrook@gmail.com>
2017-06-19 18:57:43 -0400

    Update dependencies-mac.sh

--- setenv.sh
+++ setenv.sh
@@ -7,10 +7,10 @@ export CLANG_VERSION=3.9
 
 if uname -a | grep -q -i darwin; then
     export JAVA_HOME=$(/usr/libexec/java_home)
-    export ANDROID_NDK=/usr/local/opt/android-ndk
-    export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
-    export ANDROID_SDK=/usr/local/opt/android-sdk
-    export ANDROID_HOME=/usr/local/opt/android-sdk
+    export ANDROID_NDK=/usr/local/share/android-ndk
+    export ANDROID_NDK_HOME=/usr/local/share/android-ndk
+    export ANDROID_SDK=/usr/local/share/android-sdk
+    export ANDROID_HOME=/usr/local/share/android-sdk
 else
     export MAVEN_VERSION=3.3.9
     export GRADLE_VERSION=3.4.1
