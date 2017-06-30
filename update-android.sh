cff9cf150a2e42df185c05e5fcb9a3418fb67719
joshjdevl <joshjdevl@gmail.com>
2017-06-29 23:27:53 -0700

    fix colon to semicolon

--- update-android.sh
+++ update-android.sh
@@ -15,7 +15,7 @@ echo y | sdkmanager "extras;google;m2repository"
 echo y | sdkmanager "build-tools;23.0.2"
 echo y | sdkmanager "build-tools;23.0.3" 
 echo y | sdkmanager "build-tools;25.0.2" 
-echo y | sdkmanager "build-tools:26.0.0"
+echo y | sdkmanager "build-tools;26.0.0"
 
 echo y | sdkmanager "platforms;android-16" 
 echo y | sdkmanager "platforms;android-21" 
