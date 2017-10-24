7ecb8b1eec454c4c99b8c5885daa360ff92405cb
joshjdevl <joshjdevl@gmail.com>
2017-10-24 09:11:31 -0700

    sdkmanager.log

--- update-android.sh
+++ update-android.sh
@@ -2,21 +2,21 @@
 
 . ./setenv.sh
 
-echo y | sdkmanager "platform-tools"
-echo y | sdkmanager "tools"
+echo y | sdkmanager "platform-tools" &> sdkmanager.log
+echo y | sdkmanager "tools"  >> sdkmanager.log 2>&1
 
 
-echo y | sdkmanager "extras;android;m2repository"
-echo y | sdkmanager "extras;google;m2repository"
+echo y | sdkmanager "extras;android;m2repository"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "extras;google;m2repository"  >> sdkmanager.log 2>&1
 
-echo y | sdkmanager "build-tools;23.0.2"
-echo y | sdkmanager "build-tools;23.0.3" 
-echo y | sdkmanager "build-tools;25.0.2" 
+echo y | sdkmanager "build-tools;23.0.2"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "build-tools;23.0.3"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "build-tools;25.0.2"   >> sdkmanager.log 2>&1
 
-echo y | sdkmanager "platforms;android-16" 
-echo y | sdkmanager "platforms;android-21" 
-echo y | sdkmanager "platforms;android-23" 
-echo y | sdkmanager "platforms;android-24" 
-echo y | sdkmanager "platforms;android-25" 
+echo y | sdkmanager "platforms;android-16"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "platforms;android-21"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "platforms;android-23"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "platforms;android-24"  >> sdkmanager.log 2>&1
+echo y | sdkmanager "platforms;android-25"  >> sdkmanager.log 2>&1
 
 sdkmanager --update
