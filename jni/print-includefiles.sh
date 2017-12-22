d7af891290b9ad1cc7b5984d5fd1021a4db4a09c
joshjdevl <joshjdevl@gmail.com>
2017-12-21 20:22:46 -0500

    relative path

--- jni/print-includefiles.sh
+++ jni/print-includefiles.sh
@@ -5,6 +5,6 @@ src="../libsodium/src/libsodium/include/sodium"
 ls "${src}" | while read line; do
 	#%include "../libsodium/src/libsodium/include/sodium/export.h"
 	#echo "%include \"${src}/${line}\""
-	echo "%include \"/usr/local/include/sodium/${line}\""
+	echo "%include \"sodium/${line}\""
 done
 
