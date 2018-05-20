318ef04d8e3cbb958edd67072a799b8f18401d0a
Maarten Billemont <lhunath@lyndir.com>
2018-05-20 10:15:11 -0400

    Accidentally deleted jnilib variable.

--- jni/jnilib.sh
+++ jni/jnilib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ev
 
-gcc -I../libsodium/src/libsodium/include -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I${JAVA_HOME}/include/darwin sodium_wrap.c -shared -fPIC -L ../libsodium/libsodium-host/lib -L/usr/local/lib -L/usr/lib -lsodium -o $jnilib
+jnilib=libsodiumjni.so
+gcc -I../libsodium/src/libsodium/include -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -I"$JAVA_HOME/include/darwin" sodium_wrap.c -shared -fPIC -L ../libsodium/libsodium-host/lib -L/usr/local/lib -L/usr/lib -lsodium -o "$jnilib"
 
