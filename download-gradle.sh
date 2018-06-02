09faa420c68ad87fe9ff95af179ac7dade249363
joshjdevl <joshjdevl@gmail.com>
2018-06-02 18:56:48 +0000

    yes sdkmanager license android subdirectory

--- download-gradle.sh
+++ download-gradle.sh
@@ -18,3 +18,7 @@ mkdir -p ${ANDROID_HOME}/licenses
 echo 8933bad161af4178b1185d1a37fbf41ea5269c55 > ${ANDROID_HOME}/licenses/android-sdk-license
 echo 601085b94cd77f0b54ff86406957099ebe79c4d6 > ${ANDROID_HOME}/licenses/android-googletv-license
 echo 33b6a2b64607f11b759f320ef9dff4ae5c47d97a > ${ANDROID_HOME}/licenses/google-gdk-license
+yes | sdkmanager --licenses
+
+mkdir -p ~/.android
+touch ~/.android/repositories.cfg
