46581779e5370eb4203201e262e0a05a2cb6cba4
joshjdevl <joshjdevl@gmail.com>
2018-06-02 18:57:25 +0000

    android 27 disable sdkmanager update

--- update-android.sh
+++ update-android.sh
@@ -12,11 +12,13 @@ echo y | sdkmanager "extras;google;m2repository"  | tee -a sdkmanager.log
 echo y | sdkmanager "build-tools;23.0.2"  | tee -a sdkmanager.log 
 echo y | sdkmanager "build-tools;23.0.3"  | tee -a sdkmanager.log 
 echo y | sdkmanager "build-tools;25.0.2"   | tee -a sdkmanager.log 
+echo y | sdkmanager "build-tools;27.0.3"   | tee -a sdkmanager.log
 
 echo y | sdkmanager "platforms;android-16"  | tee -a sdkmanager.log 
 echo y | sdkmanager "platforms;android-21"  | tee -a sdkmanager.log 
 echo y | sdkmanager "platforms;android-23"  | tee -a sdkmanager.log
 echo y | sdkmanager "platforms;android-24"  | tee -a sdkmanager.log 
 echo y | sdkmanager "platforms;android-25"  | tee -a sdkmanager.log 
+echo y | sdkmanager "platforms;android-27"  | tee -a sdkmanager.log
 
-sdkmanager --update
+#sdkmanager --update
