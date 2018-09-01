4988229931b41e41d30618c82cf75c9bd1cbef31
joshjdevl <joshjdevl@gmail.com>
2018-09-01 09:59:41 -0500

    Update build-linux.sh
    added comments

--- build-linux.sh
+++ build-linux.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -ev
 
+#exports environment variables needed for build tool paths
 . ./setenv.sh
 
+#clears and redownloads the libsodium submodule
 ./submodule-update.sh
 
+#builds libsodium for the host operating system
 ./build-libsodium-host.sh
 
+#generates the swig bindings
 gradle generateSWIGsource --full-stacktrace
+#performs the android ndk libsodium build
 gradle build --full-stacktrace
 
 pushd jni
 ./jnilib.sh
 popd
 
+#copies the libsodium shared library to the host lib path in order to be able to use host libsodium library
 sudo cp ./libsodium/libsodium-host/lib/libsodium.so /usr/lib
+#host libsodium api build
 mvn -q clean install
+#test that the host libsodium works
 ./singleTest.sh
 
-#./build-kaliumjni.sh
-#./build-libsodiumjni.sh
